5a031a17288d573f029558407566f982a2eaf869
helina belete <helinabmeaza@gmail.com>
2022-09-28 19:20:50 +0300

    new python commited

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -5,6 +5,6 @@ import json
 
 
 def from_json_string(my_str):
-    """returns an object (Python data structure) represented by a JSON string"""
+    """Rreturns an object (Python data structure) represented by a JSON string.
+    """
     return json.loads(my_str)
-
