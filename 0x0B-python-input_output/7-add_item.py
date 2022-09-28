e32b781ddcb2c5b9600641b7c073380af97e6a7c
helina belete <helinabmeaza@gmail.com>
2022-09-28 19:40:59 +0300

    new python commited

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,9 @@
-#!usr/bin/python3
-"""A '7-add_item' function"""
+#!/usr/bin/python3
+"""
+"A '7-add_item' function
+"""
 
 from sys import argv
-
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
@@ -10,7 +11,6 @@ filename = "add_item.json"
 
 try:
     json_list = load_from_json_file(filename)
-
 except:
     json_list = []
 
