bb931bd4da4be04c846c199a23f6c8e4c0efb5cc
helina belete <helinabmeaza@gmail.com>
2022-09-28 22:09:45 +0300

    new python commited

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,9 +3,9 @@
 import sys
 
 if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
     load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
+        __import__('8-load_from_json_file').load_from_json_file
 
     try:
         items = load_from_json_file("add_item.json")
@@ -13,39 +13,3 @@ if __name__ == "__main__":
         items = []
     items.extend(sys.argv[1:])
     save_to_json_file(items, "add_item.json")
-
-# !/usr/bin/python3
-"""
-Contains the clas "Student"
-"""
-
-
-class Student:
-    """Representation of a student"""
-
-    def __init__(self, first_name, last_name, age):
-        """Initializes the student"""
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
-
-    def to_json(self, attrs=None):
-        """returns a dictionary representation of a Student instance
-        with specified attributes"""
-        if attrs is None:
-            return self.__dict__
-        new_dict = {}
-        for a in attrs:
-            try:
-                new_dict[a] = self.__dict__[a]
-            except:
-                pass
-        return new_dict
-
-    def reload_from_json(self, json):
-        """replaces all attributes of the Student instance"""
-        for key in json:
-            try:
-                setattr(self, key, json[key])
-            except FileNotFoundError:
-                pass
