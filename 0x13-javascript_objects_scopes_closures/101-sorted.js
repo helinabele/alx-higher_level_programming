735431b6dfd524f18328231113f62e6562eb11df
helina belete <helinabmeaza@gmail.com>
2022-11-30 05:22:19 +0300

    js objects scopes closures

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,10 +1,11 @@
-i#!/usr/bin/node
-const { dict } = require('./101-data.js').dict;
-const Dictn = {};
-for (const N in dict) {
-    if (Dictn[dict[N]] === undefined) {
-	Dictn[dict[N]] = [N];
-    }
-    Dictn[dict[N]].push(N);
+#!/usr/bin/node
+const dict = require('./101-data.js').dict;
+let newDict = {};
+for (let key in dict) {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [key];
+  } else {
+    newDict[dict[key]].push(key);
+  }
 }
-console.log(Dictn);
+console.log(newDict);
