4f83510eea4826cebab5830bc9ff7ddf3e3e8676
helina belete <helinabmeaza@gmail.com>
2022-12-16 18:17:03 +0300

    making files excutable

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-"""contains the function find_peak"""
+"""Defines a peak-finding algorithm."""
 
 
 def find_peak(list_of_integers):
-    """finds a peak in a list of unsorted integers"""
-    li = list_of_integers
-    l = len(li)
-    if l == 0:
-        return
-    m = l // 2
-    if (m == l - 1 or li[m] >= li[m + 1]) and (m == 0 or li[m] >= li[m - 1]):
-        return li[m]
-    if m != l - 1 and li[m + 1] > li[m]:
-        return find_peak(li[m + 1:])
-    return find_peak(li[:m])
+    """Return a peak in a list of unsorted integers."""
+
+    my_list = list_of_integers
+
+    if my_list:
+        my_list.sort()
+        return my_list[-1]
+    else:
+        return None
