a94d3b0d8a183be8eb4340c1918172bfb807d8aa
helina belete <helinabmeaza@gmail.com>
2023-01-18 13:09:50 +0300

    API

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,27 +1,21 @@
 #!/usr/bin/node
-const request = require('request');
-const film = process.argv[2];
-let url = 'http://swapi.co/api/people/';
-function filmcharacters (film, url) {
-  request(url, function (err, response, body) {
-    if (err) {
-      console.log(err);
-    } else if (response.statusCode === 200) {
-      let jsonobj = JSON.parse(body);
-      let people = jsonobj.results;
-      for (let i in people) {
-        for (let j in people[i].films) {
-          if (people[i].films[j].includes(film)) {
-            console.log(people[i].name);
-          }
-        }
+
+const req = require('request');
+const id = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/';
+req.get(url + id, function (error, res, body) {
+  if (error) {
+    console.log(error);
+  }
+  const data = JSON.parse(body);
+  const dd = data.characters;
+  for (const i of dd) {
+    req.get(i, function (error, res, body1) {
+      if (error) {
+        console.log(error);
       }
-      if (jsonobj.next !== null) {
-        filmcharacters(film, jsonobj.next);
-      }
-    } else {
-      console.log('An error occured. Status code: ' + response.statusCode);
-    }
-  });
-}
-filmcharacters(film, url);
+      const data1 = JSON.parse(body1);
+      console.log(data1.name);
+    });
+  }
+});
--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,15 +1,24 @@
 #!/usr/bin/node
+
 const request = require('request');
-const endpoint = process.argv[2];
-let count = 0;
+const url = process.argv[2];
 
-request(endpoint, (err, res, body) => {
-  if (err) throw err;
-
-  JSON.parse(body).results.forEach(result => {
-    result.characters.forEach(character => {
-      if (character.endsWith('18/')) count++;
-    });
-  });
-  console.log(count);
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const films = JSON.parse(body).results;
+    let count = 0;
+    for (const filmIndex in films) {
+      const filmChars = films[filmIndex].characters;
+      for (const charIndex in filmChars) {
+        if (filmChars[charIndex].includes('18')) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
+  }
 });
